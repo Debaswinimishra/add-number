7a9d6972cd0577ef8b393042354de2af221d3828
Debaswini Mishra <115451804+Debaswinimishra@users.noreply.github.com>
2025-07-29 11:17:38 +0530

    comment cut

--- src/pages/BlockWiseNumberAdd.js
+++ src/pages/BlockWiseNumberAdd.js
@@ -242,26 +242,6 @@ const BlockWiseGroupFetch = () => {
       )}
 
       <div style={styles.headerContainer}>
-        {/* <button
-          onClick={handleGoBack}
-          style={styles.backButton}
-          aria-label="Go back"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="19" y1="12" x2="5" y2="12"></line>
-            <polyline points="12 19 5 12 12 5"></polyline>
-          </svg>
-        </button> */}
         <div>
           <h1 style={styles.header}>Block-wise Mobile Number Management</h1>
           <p style={styles.subHeader}>
